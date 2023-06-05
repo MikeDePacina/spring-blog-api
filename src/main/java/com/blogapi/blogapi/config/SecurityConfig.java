3645ec02ffa0b8ab0133a0ab70791337b9c43fe3
MikeDePacina <mikedpacina@gmail.com>
2023-06-05 16:23:38 -0700

    implemented authentication from user and role fetched from database

--- src/main/java/com/blogapi/blogapi/config/SecurityConfig.java
+++ src/main/java/com/blogapi/blogapi/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.blogapi.blogapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -17,6 +19,16 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    private UserDetailsService userDetailsService;
+
+    SecurityConfig(UserDetailsService userDetailsService){
+        this.userDetailsService = userDetailsService;
+    }
+
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     public static PasswordEncoder passwordEncoder(){
@@ -32,20 +44,20 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails mike = User.builder()
-                .username("mike")
-                .password(passwordEncoder().encode("loona"))
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(mike,admin);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//        UserDetails mike = User.builder()
+//                .username("mike")
+//                .password(passwordEncoder().encode("loona"))
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("admin"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(mike,admin);
+//    }
 }
