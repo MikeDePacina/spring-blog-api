172b588c1c07179630f3bf9020c7df160a5d8539
MikeDePacina <mikedpacina@gmail.com>
2023-06-06 23:47:05 -0700

    figured JWT in spring boot

--- src/main/java/com/blogapi/blogapi/config/SecurityConfig.java
+++ src/main/java/com/blogapi/blogapi/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.blogapi.blogapi.config;
 
+import com.blogapi.blogapi.security.JwtAuthenticationEntryPoint;
+import com.blogapi.blogapi.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,20 +11,31 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
 
     private UserDetailsService userDetailsService;
+    private JwtAuthenticationEntryPoint authenticationEntryPoint;
 
-    SecurityConfig(UserDetailsService userDetailsService){
+    private JwtAuthenticationFilter authenticationFilter;
+
+
+    SecurityConfig(UserDetailsService userDetailsService,
+                   JwtAuthenticationEntryPoint authenticationEntryPoint,
+                   JwtAuthenticationFilter authenticationFilter){
+
         this.userDetailsService = userDetailsService;
+        this.authenticationEntryPoint = authenticationEntryPoint;
+        this.authenticationFilter = authenticationFilter;
     }
 
 
@@ -43,7 +56,13 @@ public class SecurityConfig {
                         authorize.requestMatchers(HttpMethod.GET,"/api/**").permitAll()
                                 .requestMatchers("/api/auth/**").permitAll()
                                 .anyRequest().authenticated()
-                ).httpBasic(Customizer.withDefaults());
+                ).exceptionHandling( exception -> exception
+                        .authenticationEntryPoint(authenticationEntryPoint)
+                ).sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+        http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
